515a2ac8a7801c96a68f2c33c75e0c3d9e0167b3
Sven Ulland <sven.ulland@gmail.com>
2022-03-07 20:31:39 +0100

    Parse Nginx 'excess' from log event

--- nginx_limitreq_ipset/tail.py
+++ nginx_limitreq_ipset/tail.py
@@ -14,14 +14,31 @@ class StdStreamType(Enum):
     STDERR = 2
 
 
+class LimitReqAction(Enum):
+    LIMIT = 1
+    DELAY = 2
+
+
 class UnhandledEventException(Exception):
     pass
 
 
 def parse_limit_req(s):
-    m = re.match(r".*\blimiting requests\b", s)
+    m = re.match(r".*\b(?P<action>limiting|delaying) requests\b", s)
     if not m:
         raise UnhandledEventException("not a limit_req event")
+
+    action_str = m.group("action")
+    if action_str == "limiting":
+        action = LimitReqAction.LIMIT
+    elif action_str == "delaying":
+        action = LimitReqAction.DELAY
+
+    m = re.match(r".*\bexcess: (?P<excess>[\d.]+)", s)
+    if not m:
+        raise UnhandledEventException("excess not parsed")
+
+    excess = m.group("excess")
 
     m = re.match(r'.*\bzone "(?P<zone>[^"]+)"', s)
     if not m:
@@ -29,21 +46,23 @@ def parse_limit_req(s):
 
     zone = m.group("zone")
 
+    dry_run = False
+    m = re.match(r".*\bdry run\b", s)  # nginx 1.17.1 and later
+    if m:
+        dry_run = True
+
     m = re.match(r".*\bclient: (?P<addr>[^,]+),", s)
     if not m:
         raise UnhandledEventException("client addr not parsed")
 
     addr = m.group("addr")
 
-    dry_run = False
-    m = re.match(r".*\bdry run\b", s)  # nginx 1.17.1 and later
-    if m:
-        dry_run = True
-
     return {
+        "action": action,
+        "excess": excess,
         "zone": zone,
-        "addr": addr,
         "dry_run": dry_run,
+        "addr": addr,
     }
 
 
